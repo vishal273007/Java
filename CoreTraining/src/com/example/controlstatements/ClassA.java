def2262e741bb75a78c086c5b65c8de53c675785
Vishal Vishwakarma <vv273007@gmail.com>
2024-11-26 18:57:39 +0530

    updated java initial control statement files

---
+++ CoreTraining/src/com/example/controlstatements/ClassA.java
@@ -0,0 +1,15 @@
+package com.example.controlstatements;
+
+public class ClassA {
+	void meth1(int i) {
+		System.out.println("meth1() called.");
+		if(!(i<'A')) {
+			System.out.println("if block executed");
+		}
+		System.out.println("meth1() execution complete.");
+	}
+	
+	void meth2(int i) {
+		
+	}
+}
