ea280277be9bf81e568fe1d9f331f5726a1a76cd
Vishal Vishwakarma <vv273007@gmail.com>
2024-12-04 00:05:56 +0530

    for loop program updated

---
+++ CoreTraining/src/controlstatements/iteration/Ex4ForLoopEvenNoPrinting.java
@@ -0,0 +1,26 @@
+
+// Java program to print even numbers between two given numbers and count of total numbers.
+
+package controlstatements.iteration;
+
+public class Ex4ForLoopEvenNoPrinting {
+	void meth1(int x, int y) {
+		int count = 0; // count is for counting the total no of even numbers that will be printed on
+						// consol.
+		for (; x <= y; x++) {
+			if (x % 2 == 0) {
+				System.out.println(x + " is even.");
+				count++;
+			}
+
+			System.out.println("---------------------");
+		}
+
+		System.out.println("\nThere are total " + count + " even numbers.");
+	}
+
+	public static void main(String[] args) {
+		Ex4ForLoopEvenNoPrinting aobj = new Ex4ForLoopEvenNoPrinting();
+		aobj.meth1(10, 20);
+	}
+}
---
+++ CoreTraining/src/controlstatements/iteration/Ex6ForLoopAndBreak.java
@@ -0,0 +1,19 @@
+
+// Java code for for loop with break statement.
+
+package controlstatements.iteration;
+
+public class Ex6ForLoopAndBreak {
+	void display() {
+		int a = 10;
+		for (int k = (a++) + (++a) + (++a); k > 10; k++) {
+			System.out.println(k);
+			break;
+		}
+	}
+
+	public static void main(String[] args) {
+		Ex6ForLoopAndBreak a = new Ex6ForLoopAndBreak();
+		a.display();
+	}
+}
